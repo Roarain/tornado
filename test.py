5be3efc768a2b27696622dadc5bccb84d5313552
welovewxy@gmail.com <GITwXy*@1)*9@5*>
2018-01-10 15:34:53 +0800

    第二次提交tornado代码

---
+++ test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+@purpose: 
+@version: 1.0
+@author: Roarain
+@time: 2018/1/10 15:34
+@contact: welovewxy@126.com
+@file: test.py
+@license: Apache Licence
+@site: 
+@software: PyCharm
+"""
+
+import logging
+
+logging.basicConfig(filename='test.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(level=logging.DEBUG)
+
+print 1+1
